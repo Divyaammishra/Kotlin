a478db93ea11e2a61cb5e66c3543a8165ceefda1
Divyaam Mishra <78863278+Divyaammishra@users.noreply.github.com>
2024-01-05 08:49:19 +0530

    Add files via upload

---
+++ Kotlin_one.kt
@@ -0,0 +1,13 @@
+/*
+In this module, we will see how to print " Hello World " in the Kotlin
+ */
+
+fun main() { //This is the starting of any Kotlin program, this is the main function
+    println("Hello World") //Style 1
+    print("Hello World") //Style 2
+
+    /*
+    print with ln is having same functionality as Java, it will print an extra line after the statement.
+    Only print won't print any extra line after the statement.
+     */
+}
