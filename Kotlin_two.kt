e7e3f0cbc9e3c1afa5cfe000a9e7a5b0a2ff7e0f
Divyaam Mishra <78863278+Divyaammishra@users.noreply.github.com>
2024-01-06 20:09:50 +0530

    Add files via upload

---
+++ Kotlin_two.kt
@@ -0,0 +1,17 @@
+/*
+In this module, we will see Kotlin arithmetic operations
+ */
+
+fun main() {
+
+    //First Way
+    println(1 + 2)
+    println(2 - 2)
+    println(3 * 2)
+    println(4 / 2)
+
+    //Second Way
+    println(1.plus(2)) //Addition
+    println(2.times(2)) //Multiplication
+    println(4.div(2)) //Division
+}
---
+++ Main.kt
@@ -0,0 +1,8 @@
+/*
+Hello This Is A Kotlin Crash Course.
+From Beginners to Intermediate to Advance.
+ */
+
+fun main() {
+
+}
