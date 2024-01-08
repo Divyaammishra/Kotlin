ef1c7700f9ee4d4fa4ad41471a591da4d8c09752
Divyaam Mishra <78863278+Divyaammishra@users.noreply.github.com>
2024-01-08 21:34:27 +0530

    Add files via upload

---
+++ Kotlin_three.kt
@@ -0,0 +1,15 @@
+//Conditional Statement
+
+fun main() {
+
+    //1 -> If-Else
+    val a = 56
+    val b = 54
+    val c = 60
+
+    if(a > b) {
+        println("A Won")
+    } else {
+        println("B Won")
+    }
+}
